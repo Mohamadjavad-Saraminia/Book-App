f9585d5ada3c96f80795431e685f35fea8aa8404
Mj Saraminia <mjsaraminiadeveloped@gmail.com>
2024-01-01 22:46:18 +0330

    Styles Css Final

--- src/Component/Searchbox.jsx
+++ src/Component/Searchbox.jsx
@@ -1,10 +1,12 @@
 import { IoSearchSharp } from "react-icons/io5";
+import styles from "../Component/Searchbox.module.css"
 
 // eslint-disable-next-line react/prop-types
 function Searchbox({ search, setSearch, searchHandeler }) {
     return (
         <>
             <input
+                className={styles.search}
                 type="text"
                 placeholder="Search title"
                 value={search}
