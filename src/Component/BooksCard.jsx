920623a9dd21402a714874a25862c5fc910b0895
Mj Saraminia <mjsaraminiadeveloped@gmail.com>
2024-01-01 05:07:12 +0330

    Style Css BookCard Component & logic Of Like Button

--- src/Component/BooksCard.jsx
+++ src/Component/BooksCard.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react"
+import { AiFillHeart } from "react-icons/ai"
 
+import styles from "./BooksCard.module.css"
 
 // eslint-disable-next-line react/prop-types
 function BooksCard({ data: { title, author, image, language, pages } }) {
-    return (
-        <div>
 
+    const [like, setLike] = useState(false);
+
+    const likeHandeler = () => {
+        setLike((like) => !like);
+    }
+
+    return (
+        <div className={styles.card}>
             <img src={image} alt="title" />
-            <div>
+
+            <div className={styles.info}>
                 <h3>{title}</h3>
                 <p>{author}</p>
-            </div>
 
-            <div>
-                <span>{language}</span>
-                <span>{pages}</span>
+                <div>
+                    <span>{language}</span>
+                    <span>{pages}</span>
+                </div>
             </div>
-            <button>like</button>
+            <button onClick={likeHandeler}>
+                <AiFillHeart color={like ? "red" : "#e0e0e0"} fontSize="1.8rem" />
+            </button>
         </div>
     )
 }
